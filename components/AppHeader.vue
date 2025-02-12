516b4964130f44de3cb6f75e1f656b1f2675515c
Michael Briquet <contact@michaelbr-dev.fr>
2025-02-12 15:14:51 +0100

    🔨 Add links on icons, alt propoerties on images, and style hover menu buttons

--- components/AppHeader.vue
+++ components/AppHeader.vue
@@ -1,17 +1,22 @@
 <template>
   <v-app-bar :elevation="0" color="black" class="pa-3">
     <template #prepend>
-      <v-img
-        class="mr-1 ml-8"
-        src="../public/assets/logo-tel.png"
-        height="64"
-        width="64"
-        contain
-      ></v-img>
+      <a href="/">
+        <v-img
+          class="mr-1 ml-8"
+          src="../public/assets/logo-tel.png"
+          alt="Logo Allo Pizza"
+          height="64"
+          width="64"
+          contain
+        ></v-img>
+      </a>
     </template>
     <v-app-bar-title>
       <div class="d-flex ga-5">
-        <span class="website_title main align-self-center">ALLO PIZZA</span>
+        <a href="/">
+          <span class="website_title main align-self-center">ALLO PIZZA</span>
+        </a>
         <div class="d-inline-flex flex-wrap">
           <span class="website_title sub d-none d-lg-block">2 rue Jean Jaurès</span>
           <span class="website_title sub d-none d-lg-block">59880 Saint-Saulve</span>
@@ -20,14 +25,19 @@
     </v-app-bar-title>
 
     <v-toolbar-items v-if="mdAndUp" class="align-center">
-      <v-btn v-for="(item, i) in items" :key="i" :href="item.route">{{ item.title }}</v-btn>
-      <v-img
-        class="mr-2"
-        src="../public/assets/icone-fb.png"
-        height="32"
-        width="32"
-        contain
-      ></v-img>
+      <v-btn v-for="(item, i) in items" :key="i" :href="item.route" class="rounded-lg menu-btn">
+        {{ item.title }}
+      </v-btn>
+      <a href="https://www.facebook.com/profile.php?id=100083018124674" target="_blank">
+        <v-img
+          class="mr-2"
+          src="../public/assets/icone-fb.png"
+          alt="Logo Facebook"
+          height="32"
+          width="32"
+          contain
+        ></v-img>
+      </a>
     </v-toolbar-items>
 
     <v-toolbar-items v-if="smAndDown" class="align-right">
@@ -62,10 +72,12 @@ const items = [
 <style scoped>
 .website_title {
   font-size: 30px;
+  text-decoration: none;
 }
 
 .website_title.main {
   font-size: clamp(20px, 1.8vw, 30px);
+  text-decoration: none;
 }
 
 .website_title.sub {
@@ -79,5 +91,15 @@ const items = [
 .v-toolbar .v-btn {
   font-weight: bold;
   font-size: clamp(14px, 1vw, 18px);
+}
+a:link {
+  text-decoration: none;
+}
+a:visited {
+  text-decoration: none;
+  color: white;
+}
+.menu-btn:hover {
+  color: #cc0033;
 }
 </style>
