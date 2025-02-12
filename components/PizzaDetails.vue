f0e9480fa136c75e5b9b8cc78b0a814dce522337
Michael Briquet <contact@michaelbr-dev.fr>
2025-02-12 15:17:45 +0100

    ✨ Add detail pizza page

---
+++ components/PizzaDetails.vue
@@ -0,0 +1,15 @@
+<template>
+  <v-card variant="tonal" class="rounded-lg w-75 mx-auto my-7 text-center text-white">
+    <v-card-title>{{ props.pizza.name }} - {{ props.pizza.price }}</v-card-title>
+    <v-card-subtitle>{{ props.pizza.subName }}</v-card-subtitle>
+    <v-card-text>{{ props.pizza.ingredients }}</v-card-text>
+    <v-img class="ma-2 rounded-lg" :src="assetPath + props.pizza.img" />
+  </v-card>
+</template>
+
+<script setup>
+const assetPath = '/assets/pizzas/';
+const props = defineProps({
+  pizza: { type: Array, required: true },
+});
+</script>
---
+++ pages/[id].vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <HeroContainer :image="image" />
+    <PizzaDetails :pizza="pizza" />
+  </div>
+</template>
+
+<script setup>
+import image from '../public/assets/photos/hero-pizzas.jpeg';
+import { ref } from 'vue';
+import { useRoute } from 'vue-router';
+import PizzaDetails from '~/components/PizzaDetails.vue';
+import allopizzaData from '~/public/allopizza.json';
+
+const route = useRoute();
+const pizzaId = route.params.id;
+const pizza = ref(allopizzaData.pizzas.find((p) => p.id === Number(pizzaId)));
+</script>
